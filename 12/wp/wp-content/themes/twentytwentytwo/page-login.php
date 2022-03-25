d118344607ed8610afa92cc98f67c40ae8e9ba98
shene_hase <shane.hasebe@gmail.com>
2022-03-25 14:20:06 +0900

    no message

---
+++ 12/wp/wp-content/themes/twentytwentytwo/page-login.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Template Name: page-user-login
+*/
+
+echo do_shortcode('[wpmem_form login]');
+
+?>
+
+<?php echo do_shortcode('[wpmem_logout]ログアウトはこちら[/wpmem_logout]');?>
---
+++ 12/wp/wp-content/themes/twentytwentytwo/page-register.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Template Name: page-user-regist
+*/
+
+echo do_shortcode('[wpmem_form register]');
+?>
+
+<?php echo do_shortcode('[wpmem_loginout]ログインはこちら[/wpmem_loginout]');?>
