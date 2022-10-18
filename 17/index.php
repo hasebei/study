bbf39ab25f01726b2cf3865406b2e9bb19788e8e
hasebe <hase.info@gmail.com>
2022-10-18 15:30:00 +0900

    no message

---
+++ 17/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<style>
+    .insta{
+        width: 30%;
+    }
+    .insta iframe{
+        width: 100% !important;
+    }
+</style>
+</head>
+<body>
+<div class="insta">
+<!-- SnapWidget -->
+<iframe src="https://snapwidget.com/embed/1013554" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden;  width:320px; height:5290px"></iframe>
+</div>
+</body>
+</html>
